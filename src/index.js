3fa4c8ad175b7ae8b9b73d5559f66593e6fc3d2f
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-14 20:48:05 +0200

    Update port number

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(multer({ dest: 'uploads' }).single('image'));
 app.use(router)
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
 });
 
 export default app;
